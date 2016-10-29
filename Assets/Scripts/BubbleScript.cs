455cc2aa537ea66c44800edc48c914d6e7332bea
Nico Rößel <nicoroessel@gmail.com>
2016-10-29 11:48:32 +0200

    Unnötiger code weg

--- Assets/Scripts/BubbleScript.cs
+++ Assets/Scripts/BubbleScript.cs
@@ -10,7 +10,6 @@ public class BubbleScript : MonoBehaviour {
 	const float maxThresholdRight = 5.0f;
 
 	float baseSpeed;
-	Vector3 pos;
 	Vector3 target;
 	Vector3 nextTarget;
 
@@ -18,66 +17,19 @@ public class BubbleScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
 		movement = GetComponent<MovementBehaviour>();
 		movement.defaultSpeed = 2f;
 
-		thresholdXleft = Random.Range(maxThresholdLeft, 0.0f);
-		thresholdXright = Random.Range(0.0f, maxThresholdRight);
-		thresholdYtop = Random.Range(0.0f, maxThresholdTop);
-		thresholdYbottom = Random.Range(maxThresholdBottom, 0.0f);
-=======
 		SetNewTarget ();
->>>>>>> Nico
 	}
 
 	// Update is called once per frame
 	void Update () {
 		baseSpeed = Time.deltaTime * movement.speed;
-		pos = transform.position;
 		Move ();
 
 	}
 
-<<<<<<< HEAD
-	void ToggleX(){
-		if (directionX == "left") {
-			float speed = baseSpeed;
-			if (pos.x > thresholdXleft) {
-				transform.Translate (Vector3.left * speed);
-			} else {
-				directionX = "right";
-				thresholdXright = Random.Range(0.0f, maxThresholdRight);
-			}
-		} else if (directionX == "right") {
-			float speed = baseSpeed;
-			if (pos.x < thresholdXright) {
-				transform.Translate (Vector3.right * speed);
-			} else {
-				directionX = "left";
-				thresholdXleft = Random.Range(maxThresholdLeft, 0.0f);
-			}
-		}
-	}
-
-	void ToggleY(){
-		if (directionY == "top") {
-			float speed = baseSpeed;
-			if (pos.y < thresholdYtop) {
-				transform.Translate (Vector3.up * speed);
-			} else {
-				directionY = "bottom";
-				thresholdYbottom = Random.Range(maxThresholdBottom, 0.0f);
-			}
-		} else if (directionY == "bottom") {
-			float speed = baseSpeed;
-			if (pos.y > thresholdYbottom) {
-				transform.Translate (Vector3.down * speed);
-			} else {
-				directionY = "top";
-				thresholdYtop = Random.Range(0.0f, maxThresholdTop);
-			}
-=======
 	void Move() {
 
 		float distance = Vector3.Distance (transform.position, target);
@@ -85,11 +37,7 @@ public class BubbleScript : MonoBehaviour {
 
 
 		if (distance < 1.5f) {
-			print ("before: " + target.ToString());
 			target = Vector3.Slerp (target, nextTarget , baseSpeed);
-
-			print ("after: " + target.ToString());
-
 			transform.position = Vector3.MoveTowards (transform.position, target, baseSpeed);
 
 		} else {
@@ -114,7 +62,6 @@ public class BubbleScript : MonoBehaviour {
 		target = new Vector3 (x, y, 0f);
 		while (Vector3.Distance (target, nextTarget) < 1f) {
 			SetNewTarget ();
->>>>>>> Nico
 		}
 
 	}

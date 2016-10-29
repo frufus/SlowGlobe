131c3be56f7fdae06c042ef7052a45c02f724734
Tabble <bluecher.tanja@gmail.com>
2016-10-29 14:27:19 +0200

    added: number of stages of each region

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,8 +4,49 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+    public int CurrentHeight;
+    //Anzahl an Tiefseehintergründen, wie oft sie wiederholt werden sollen
+    public int DeepSeaNumber;
+    // Anzahl an Seehintergründen
+    public int SeeNumber;
+    // Oberfläche = 1
+    public int SurfaceNumber = 1;
+    // Himmel
+    public int SkyeNumber;
+    // Übergang Himmel zu Weltall
+    public int SkyeToSpaceNumber;
+    // Weltall
+    public int SpaceNumber;
+
+
+   
+
+    private static GameController instance = null;
+    public static GameController Instance
+    {
+        get
+
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<GameController>();
+            }
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        SeeNumber = DeepSeaNumber + SeeNumber;
+        SurfaceNumber = SeeNumber + SurfaceNumber;
+        SkyeNumber = SurfaceNumber + SkyeNumber;
+        SkyeToSpaceNumber = SkyeNumber + SkyeToSpaceNumber;
+    }
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	

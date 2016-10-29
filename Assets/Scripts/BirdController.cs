d2504a1ec2f2477007de390a68ed1d9788a7ae98
Tabble <bluecher.tanja@gmail.com>
2016-10-29 02:13:44 +0200

    Birdcontroller with birds added

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -3,11 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class BirdController : MonoBehaviour {
+
     int rndNumberOfItems;
-    List<GameObject> EnemyBirds;
-    
-	// Use this for initialization
-	void Start () {
+    public List<GameObject> EnemyBirds;
+
+    private static BirdController instance = null;
+    public static BirdController Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<BirdController>();
+            }
+            return instance;
+        }
+    }
+    // Use this for initialization
+    void Start () {
         rndNumberOfItems = Random.Range(0, EnemyBirds.Count);
 	}
 	
